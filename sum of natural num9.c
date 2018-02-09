3177f317298f4a02d301d72076af0ebb1cb580da
SriHariVenkatesh <35689225+SriHariVenkatesh@users.noreply.github.com>
2018-02-09 13:57:53 +0530

    som of natural num

---
+++ sum of natural num9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int n, i, sum = 0;
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+    for(i=0;i<=0;i++ )
+    {
+        sum += i;  
+    }
+    printf ("Sum = %d",sum );
+    return 0;
+    getch();
+    
+}
