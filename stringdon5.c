467a6afda8d08f15bd95db2b28d743a08b310825
SriHariVenkatesh <35689225+SriHariVenkatesh@users.noreply.github.com>
2018-03-05 16:06:12 +0530

    stringdon5

---
+++ stringdon5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char ch[100],i,b,sum=0;
+printf ("\n Enter a string:");
+gets(ch);
+b=strlen(ch);
+for(i=0;i<b;i++)
+  {
+    if(!((0<=a[i]&&(a[i]<=9))||(('a'<=a[i]&&(a[i]<='z')))
+     sum=sum+1;
+  }
+printf ("\n The no of numerics is %d",sum);
+return0;
+}
