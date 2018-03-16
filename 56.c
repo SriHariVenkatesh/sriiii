8854774d23bb56c5ea4583bb75620274973740e4
SriHariVenkatesh <35689225+SriHariVenkatesh@users.noreply.github.com>
2018-03-16 16:01:25 +0530

    56

---
+++ 56.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+ char a[30];
+ int i;
+ scanf("%s",a);
+   for(i=0;a[i]!='\0';i++)
+     {
+        if((a[i]>='a'||a[i]<='z'||a[i]>='A'||a[i]<='Z')&&(a[i]>=0||a[i]<=9))
+          {
+             printf("yes");
+   
+          }
+        else
+          {
+             printf("no");
+           }
+    }
+}
